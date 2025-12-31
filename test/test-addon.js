0b7eac4a1cdeb5c6265629847475cf51c78c81ba
Chris McGowan <git@anthum.com>
2025-12-31 02:42:06 +0100

    add tests

--- test/test-addon.js
+++ test/test-addon.js
@@ -4,14 +4,18 @@ import { hello } from '../lib/addon.js'
 assert(hello, "undefined named import")
 testName('world', 'hello world')
 testName('everyone', 'hello everyone')
+testName(0, 'hello 0')
 testName(1, 'hello 1')
 testName(-1, 'hello -1')
 testName('1', 'hello 1')
+testName(false, 'hello false')
+testName(true, 'hello true')
 testName(null, 'hello')
 testName(undefined, 'hello')
 
 function testName(name, expected) {
-  assert.strictEqual(name ? hello(name) : hello(), expected, "Unexpected value returned")
+  const returnVal = (name == null || name == undefined) ? hello() : hello(name)
+  assert.strictEqual(returnVal, expected, "Unexpected value returned")
 }
 
 console.log("testing ✅")
